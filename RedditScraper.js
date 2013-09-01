56c61e6244329b6bb78a9422a8bfe94b0ac8358c
Scott McPhillips <mcphscott@yahoo.com>
2013-09-01 12:49:52 -0700

    refactor, moving more towards functional program instead of oop

--- RedditScraper.js
+++ RedditScraper.js
@@ -6,74 +6,38 @@ var request = require('request');
 var async = require('async');
 var fs = require ("fs");
 
-//Reddit-scraping class constructor
+//Reddit-scraping higher order function, returns
+// function for scraping in format which can be used with async commands.
 //pass in the name of the subreddit and the max number of
 //images to download. The scrape method will
 //downloads images and creates a single local web page
 //for viewing all of the images.
-module.exports = function RedditScraper ( sub_, rootImagePath_, min_, max_, existingFiles_ ) {
-	var existingFiles = existingFiles_;
-  //private variables
-	var sub = sub_;
-	var nextUrl = 'http://www.reddit.com/r/' + sub +'.json';
-
-	var max = max_;
-	var min = min_;
-	var rootPath = rootImagePath_;
-
-	var stampedName = sub + "_" + new Date().getTime();
-	var dir = rootPath + stampedName + "/";
-	
+module.exports.scraperGen = function ( sub, dir, max, filter, imageCallback ) {
 	var cnt = 0;
-	//add a foundation.css to the page header
-	var myPage = "<head><meta charset=\"utf-8\" />\n"+
-		"<meta name=\"viewport\" content=\"width=device-width\" />\n"+
-		"<title>"+sub+"</title>\n"+
-		"<link rel=\"stylesheet\" href=\"../../css/foundation.min.css\" /></head>\n";
-	var activeDownloads = 0;
-	
-	var createDir = function( callback_ ) {
-		fs.exists(dir, function (exists) {
-			if (!exists) {
-				fs.mkdir( dir, function ( err ) {
-					if ( err ) {
-						console.log("Could not create images directory (" + dir+ "):" +err);
-					}
-					callback_(err);
-				});
-			} else {
-				callback_();
-			}
-		});
-	};
 
-	//public methods	
-	this.scrape = function( callback_ ) {
-		async.series([ createDir, requestAllUrls, writePage]);
-	};
-	
-	var writePage = function ( callback_ ) {
-		//write the html page for local viewing
-		fs.writeFile( dir + "index.html", myPage, callback_ );	
-	};
-	
 	//public method for requesting url's
 	//recursively calls until max reached
-	var requestAllUrls = function( callback_ ) {
+	return function( callback_ ) {
+		var requestUrlFun = requestUrlGen ( sub, dir, max, filter, imageCallback );
 		async.whilst( function () {
+				cnt += 25;
 				return (cnt < max);
-			}, requestUrl,
-		function (err) {
-			myPage += "";
-			callback_(err);
-		} );
+			}, requestUrlFun,
+			function (err) {
+				callback_(err);
+			}
+		);
 	};
+};
 	
-	var requestUrl = function( callback_ ) {
+var requestUrlGen = function ( sub, dir, max, filter, imageCallback) {
+	var nextUrl = 'http://www.reddit.com/r/' + sub +'.json';
+	var activeDownloads = 0;
+ 
+	return function( callback_ ) {
 		console.log(nextUrl);
 		
 		request.get( nextUrl, function(error, res, json) {
-
 			if ( error || res.statusCode != 200) {
 				console.log(error); // Print the web page.
 				callback_(error);
@@ -84,33 +48,13 @@ module.exports = function RedditScraper ( sub_, rootImagePath_, min_, max_, exis
 			var children = listing.data.children;
 			
 			var imageLinks = children.filter( function( i ) {
-				var href = i.data.url;
-				console.log(href);
-
-				var lastDotIndex = href.lastIndexOf(".");
-				var fileType = href.slice ( lastDotIndex);
-				return (fileType == ".gif" || fileType == ".jpg" || fileType == ".png");
+				return filter( i );
 			});
 			
 			imageLinks.forEach( function (i) {
-			
-				if ( cnt < min ) return;
-
 				var href = i.data.url;
 				var text = i.data.title;
-				
-				var imageName = href.substr(href.lastIndexOf('/') + 1);
-
-				if (i.data.over_18) {
-					console.log("skipping over 18 images!");
-					return;
-				}
-				
-				if ( existingFiles.indexOf(imageName) != -1) {
-					console.log("skipping " + imageName);
-					return;
-				}
-				
+				var imageName = href.substr(href.lastIndexOf('/') + 1);							
 				var localImageName = imageName;
 				var localImagePath = dir + localImageName;
 				
@@ -126,20 +70,16 @@ module.exports = function RedditScraper ( sub_, rootImagePath_, min_, max_, exis
 					}
 				}).pipe(fs.createWriteStream( localImagePath ));				
 				
-				myPage += "<div class=\"row\"><div class=\"large-12 columns\">";
-				myPage += text+"<img src=\"" + localImageName+"\"></div></div>";				
+				imageCallback(localImageName, text);
 			});
 								
 			var after = listing.data.after;
 			//nextUrl = $("p.nextprev a[rel^='nofollow next']").attr("href");			
 			nextUrl = 'http://www.reddit.com/r/' + sub +'.json?after='+after;
 				
-			cnt += 25;
 			callback_();
 		});
 		
 	};
 };
-
-
 
--- scrape.js
+++ scrape.js
@@ -6,27 +6,73 @@
 
 var RedditScraper = require('./RedditScraper.js');
 var FileUtility = require('./FileUtility.js');
+var async = require('async');
 var argv = require('optimist')
 	.usage("Usage: $0 -r [subreddit]")
 	.demand(['r'])
 	.default('library',"./images")
 	.default('pages', 4).argv;
 
+var sub = argv.r;
 var nItems = argv.pages * 25;
 
 var existingFiles = [];
+
+var rootPath = argv.library + '/';
+var stampedName = sub + "_" + new Date().getTime();
+var dir = rootPath + stampedName + "/";
+
+//add a foundation.css to the page header
+var myPage = ["<head><meta charset=\"utf-8\" />",
+		"<meta name=\"viewport\" content=\"width=device-width\" />",
+		"<title>"+sub+"</title>",
+		"<link rel=\"stylesheet\" href=\"../../css/foundation.min.css\" /></head>"];
+
+var imageCallback = function ( localImageName, text ) {
+	myPage.push("<div class=\"row\"><div class=\"large-12 columns\">");
+	myPage.push( text );
+	myPage.push("<img src=\"" + localImageName+"\"></div></div>");				
+}
+
+//this is the filter for image items
+var filter = function( i ) {
+	var href = i.data.url;
+	var text = i.data.title;			
+	
+	var lastDotIndex = href.lastIndexOf(".");
+	var fileType = href.slice ( lastDotIndex);
+	if (fileType != ".gif" && fileType != ".jpg" && fileType != ".png") return false;
+
+	if ( i.data.over_18 ) return false;
+	
+	var imageName = href.substr(href.lastIndexOf('/') + 1);
+	if ( existingFiles.indexOf(imageName) != -1) return false;
+	
+	return true;
+}
+
+//generate function that intercepts functions that return true/false and logs the value
+var booleanLogger = function ( fun, trueLog, falseLog ) {
+	return function ( i ) {
+		var result = fun(i);
+		if (result) {
+			trueLog(i);
+		} else {
+			falseLog(i);		
+		}
+		return result;
+	}
+}
+
+var imageFilter = booleanLogger(filter,function(i) {console.log("found "+ i.data.url);}, function(i) {console.log("skipping "+ i.data.url);});
+var createDirFun = FileUtility.createDir( dir );
+var scraperFun = RedditScraper.scraperGen( sub, dir, nItems, imageFilter, imageCallback);
+var writeLocalPageFun = FileUtility.writeFile ( dir + "index.html", myPage );
+
 FileUtility.deepFileListing( argv.library + "/", function( item) {
 	existingFiles.push(item);
 },
 function (err) {
-	console.log(err);
-	console.log("adding: " + existingFiles );
-	//Try it with the aww subreddit, the only safe subreddit
-	var scraperAww = new RedditScraper( argv.r, argv.library + "/", 0 , nItems, existingFiles);
-
-	scraperAww.scrape( function ( err ) {
-	console.log('done');
-	});
-	
+	async.series( [createDirFun, scraperFun, writeLocalPageFun] );	
 });
 
